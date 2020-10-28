74b009a2ef1716d68ce9d0f962b6b6fafe2fdc71
theouternet <jrougeau@gmail.com>
2020-10-28 02:24:10 -0500

    App pt 1

--- app.js
+++ app.js
@@ -3,13 +3,44 @@ const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
+const { debugPort } = require('process');
 const app = express();
 
+//Avoid collision with frontend
 const port = 5000;
 
+//Db connection const
 const db = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'password',
     database: 'chad'
+});
+module.exports =  db;
+
+
+//Db connect act
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Db connection successful');
+});
+global.db = db;
+
+
+// Middleware
+app.set('port', process.env.port || port);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
+
+//Routes
+
+// Listen on port
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
 });
